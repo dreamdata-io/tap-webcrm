45819c4921ef85feec64efdf7dac723c0f904a5c
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-09-30 16:21:39 +0200

    show that it works and how

--- tap_webcrm/__init__.py
+++ tap_webcrm/__init__.py
@@ -1,5 +1,22 @@
+import os
+
+from tap_webcrm.client import WebCRM
+
+API_TOKEN = os.environ["WEBCRM_API_TOKEN"]
+
+webcrm_client = WebCRM(API_TOKEN)
+
+
 def main():
-    print("hello world")
+    for person in webcrm_client.list_persons():
+        print(person)
+        break
+    for opportunity in webcrm_client.list_opportunities():
+        print(opportunity)
+        break
+    for org in webcrm_client.list_organisations():
+        print(org)
+        break
 
 
 if __name__ == "__main__":
