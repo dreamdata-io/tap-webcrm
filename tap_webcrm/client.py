bf1219555ffe35c052b79eec702839d5bf6931c2
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-10-01 10:53:18 +0200

    use RefreshToken after 3000 seconds instead

--- tap_webcrm/client.py
+++ tap_webcrm/client.py
@@ -22,7 +22,9 @@ class WebCRM:
                 )
             ),
         )
-        self.__token = api_token
+        self.__api_token = api_token
+        self.__token = None
+        self.__refresh_token = None
 
     def list_persons(self):
         yield from self.__paginate("/Persons")
@@ -48,21 +50,27 @@ class WebCRM:
 
     # TTL for the token is 3600 - set it to 3000 to make sure we don't end up
     # in a situation where it has run out by milliseconds
-    @cached(cache=TTLCache(1, ttl=3000))
-    def __get_token(self, api_token):
-        # circumvent the normal self.request that expects a token
-        resp = self.__request(
-            "POST", "/Auth/ApiLogin", files={"authCode": (None, api_token)}
-        )
+    @cached(cache=TTLCache(5, ttl=3000))
+    def __get_token(self):
+        if self.__refresh_token:
+            resp = self.__request(
+                "POST",
+                "/Auth/ApiRefresh",
+                files={"refreshToken": (None, self.__refresh_token)},
+            )
+        else:
+            resp = self.__request(
+                "POST", "/Auth/ApiLogin", files={"authCode": (None, self.__api_token)}
+            )
 
-        return resp["AccessToken"]
+        return resp["AccessToken"], resp["RefreshToken"]
 
     def request(self, method, path, **kwargs):
 
-        token = self.__get_token(self.__token)
+        self.__token, self.__refresh_token = self.__get_token()
 
         headers = kwargs.pop("headers", {})
-        headers["Authorization"] = "Bearer " + token
+        headers["Authorization"] = "Bearer " + self.__token
 
         return self.__request(method, path, headers=headers, **kwargs)
 
